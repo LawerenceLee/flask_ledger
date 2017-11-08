d5c92c2ab272686888002a444aafb546b5c4b57c
Lawerence Lee <lucifitz.edward@gmail.com>
2017-11-07 17:52:06 -0800

    Added some imports, the secret key, app.run.  Added accounts to index view.

--- flask_ledger.py
+++ flask_ledger.py
@@ -1,9 +1,24 @@
-from flask import (Flask, g, render_template, flash, redirect, url_for,
+from flask import (Flask, g, render_template,
+                   flash, redirect, url_for,
                    abort)
 
+import forms
+import models
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
 app = Flask(__name__)
+app.secret_key = "aasdfasdf;aosihasgo*(&^Uhkewjd7efI&%$iygkjbsd"
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    accounts = models.Account.select()
+    return render_template('index.html', accounts=accounts)
+
+
+if __name__ == "__main__":
+    models.initialize()
+    app.run(debug=DEBUG, host=HOST, port=PORT)
