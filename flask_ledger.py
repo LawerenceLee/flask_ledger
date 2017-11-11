4ce200263e709a3152b5d0f0cba621e908fa62ed
Lawerence Lee <lucifitz.edward@gmail.com>
2017-11-10 22:30:27 -0800

    completed create_account view

--- flask_ledger.py
+++ flask_ledger.py
@@ -2,8 +2,11 @@ from flask import (Flask, g, render_template,
                    flash, redirect, url_for,
                    abort)
 
-import forms
-import models
+from forms import CreateAccountForm
+from models import (Account, DATABASE, Entry, initialize,
+                    Transfer, )
+
+from peewee import OperationalError
 
 DEBUG = True
 PORT = 8000
@@ -13,12 +16,45 @@ app = Flask(__name__)
 app.secret_key = "aasdfasdf;aosihasgo*(&^Uhkewjd7efI&%$iygkjbsd"
 
 
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = DATABASE
+    try:
+        g.db.connect()
+    except OperationalError:
+        pass
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
+@app.route('/create_account', methods=('GET', 'POST'))
+def create_account():
+    form = CreateAccountForm()
+    if form.validate_on_submit():
+        Account.create_account(
+            name=form.name.data.strip(),
+            balance=form.balance.data,
+            accnt_type=form.accnt_type.data,
+            bank=form.bank.data.strip(),
+        )
+        flash('Account Successfully Created', category='success')
+        return redirect(url_for('index'))
+    return render_template('create_account.html', form=form)
+
+
 @app.route('/')
 def index():
-    accounts = models.Account.select()
-    return render_template('index.html', accounts=accounts)
+    accounts = Account.select()
+    entries = Entry.select()
+    return render_template('index.html', accounts=accounts, entries=entries)
 
 
 if __name__ == "__main__":
-    models.initialize()
+    initialize()
     app.run(debug=DEBUG, host=HOST, port=PORT)
